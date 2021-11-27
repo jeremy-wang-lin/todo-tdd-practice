6f6066892f5273bf5c657489b524b081a7e35529
jeremy-wang-lin <51841335+jeremy-wang-lin@users.noreply.github.com>
2021-11-27 15:50:45 +0800

    Initial Commit

---
+++ tests/unit/todo.controller.test.js
@@ -0,0 +1,28 @@
+const httpMocks = require("node-mocks-http");
+
+const TodoController = require("../../controllers/todo.controller");
+const TodoModel = require("../../models/todo.model");
+
+const newTodo = require("../mock-data/new-todo.json");
+
+TodoModel.create = jest.fn();
+
+let req, res, next;
+beforeEach(() => {
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = null;
+})
+
+describe("TodoController.createTodo", () => {
+  it("should have a creteTodo function", () => {
+    expect(typeof TodoController.createTodo).toBe("function");
+  });
+
+  it("should call TodoModel.create", () => {
+    req.body = newTodo;
+
+    TodoController.createTodo(req, res, next);
+    expect(TodoModel.create).toBeCalledWith(newTodo);
+  });
+})
